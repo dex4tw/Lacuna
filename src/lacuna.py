71409c5cf5889f127e1eb7438173b1945b81a3b6
dex <121840104+dex4tw@users.noreply.github.com>
2024-07-04 16:40:18 -0400

    Lacuna launcher bug-fix

--- src/lacuna.py
+++ src/lacuna.py
@@ -2,19 +2,22 @@
 import os, requests, ctypes
 winDLL = ctypes.windll
 user32 = winDLL.user32
-lacunaBuild = requests.get('https://raw.githubusercontent.com/dex4tw/Lacuna/main/build').text
+lacunaBuild = requests.get('https://raw.githubusercontent.com/dex4tw/Lacuna/main/base/build').text
 
 if not os.path.exists(os.getcwd() + "/base"):
     os.mkdir(os.getcwd() + "/base")
-    cbuild = open(os.getcwd() + "/base/build", "w")
-    cbuild.close()
+    cBuild = open(os.getcwd() + "/base/build", "w")
+    cBuild.close()
 
 if not open(os.getcwd() + "/base/build").read() == lacunaBuild:
     user32.MessageBoxW(None, f"Installing the latest build of Lacuna ({lacunaBuild})", "Lacuna", 0)
-    reqLacuna = requests.get("https://raw.githubusercontent.com/dex4tw/Lacuna/main/build/lacuna-c.exe")
+    reqLacuna = requests.get("https://github.com/dex4tw/Lacuna/raw/main/base/lacuna-c.exe")
     cLacuna = open(f"{os.getcwd()}/base/lacuna-c.exe", "wb")
     cLacuna.write(reqLacuna.content)
     cLacuna.close()
-    os.system(f'start "" "{os.getcwd()}/base/lacuna.exe"')
+    cBuild = open(os.getcwd() + "/base/build", "w")
+    cBuild.write(lacunaBuild)
+    cBuild.close()
+    os.system(f'start "" "{os.getcwd()}/base/lacuna-c.exe"')
 else:
-    os.system(f'start "" "{os.getcwd()}/base/lacuna.exe"')
+    os.system(f'start "" "{os.getcwd()}/base/lacuna-c.exe"')
