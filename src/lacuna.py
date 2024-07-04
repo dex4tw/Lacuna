0c05a68e67989117ce2453ad6870300c7c4dc333
dex <121840104+dex4tw@users.noreply.github.com>
2024-07-04 16:48:43 -0400

    Lacuna launcher completion

--- src/lacuna.py
+++ src/lacuna.py
@@ -8,6 +8,9 @@ if not os.path.exists(os.getcwd() + "/base"):
     os.mkdir(os.getcwd() + "/base")
     cBuild = open(os.getcwd() + "/base/build", "w")
     cBuild.close()
+if not os.path.exists(os.getcwd() + "/base/build"):
+    cBuild = open(os.getcwd() + "/base/build", "w")
+    cBuild.close()
 
 if not open(os.getcwd() + "/base/build").read() == lacunaBuild:
     user32.MessageBoxW(None, f"Installing the latest build of Lacuna ({lacunaBuild})", "Lacuna", 0)
@@ -20,4 +23,7 @@ if not open(os.getcwd() + "/base/build").read() == lacunaBuild:
     cBuild.close()
     os.system(f'start "" "{os.getcwd()}/base/lacuna-c.exe"')
 else:
-    os.system(f'start "" "{os.getcwd()}/base/lacuna-c.exe"')
+    if os.path.exists(os.getcwd() + "/base/lacuna-c.exe"):
+        os.system(f'start "" "{os.getcwd()}/base/lacuna-c.exe"')
+    else:
+        user32.MessageBoxW(None, f"There is an issue, please remove your 'base' directory.", "Lacuna", 0)
